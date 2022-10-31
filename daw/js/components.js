4b1130b077eac99bda26b9f38a624da478c7b800
Alberto Ramirez <57710147+arsalas@users.noreply.github.com>
2022-11-01 00:13:54 +0100

    daw

--- daw/js/components.js
+++ daw/js/components.js
@@ -2,41 +2,49 @@
  * html del coche y la carretera
  */
 const carComponent = (car) => {
-  return (`<div class="road">
+  return `<div class="road">
         <img id="car_${car.id}" src="${car.image}" />
-      </div>`);
+      </div>`;
 };
 
 /**
  * html de la carretera sin coche
  */
 const roadComponent = () => {
-  return (`<div class="road"></div>`);
+  return `<div class="road"></div>`;
+};
+
+/**
+ * Crea el componente del coche en funcion de su posicion
+ */
+const positionComponent = (position) => {
+  switch (position) {
+    case 1:
+      return "<img src='images/gold.png'>";
+    case 2:
+      return "<img src='images/silver.png'>";
+    case 3:
+      return "<img src='images/bronze.png'>";
+    default:
+      return position;
+  }
 };
 
 /**
  * Crea la tabla de html con las posiciones de cada coche
  */
 const tableComponent = () => {
-  let tableHtml = `
-  <table>
-  <tr>
-    <th></th>
-    <th>Car</th>
-    <th>Position</th>
-    <th>Distance</th>
-  </tr>`;
+  let tableHtml = `<div class="container-table">
+  <div class="table">`;
   const carSort = sortByKeyDesc(CarController.cars, "position");
-  carSort.forEach((row, index) => {
-    tableHtml += `
-    <tr>
-      <th><img src='${row.image}' /></th>
-      <th>Coche ${row.id}</th>
-      <th>${index + 1}</th>
-      <th>${row.distance}</th>
-    </tr>
-    `;
+  carSort.forEach((row) => {
+      tableHtml += `
+      <div class="row ${getClassPosition(row.position)}">
+      <div class="column"><img src='${row.image}' /></div>
+      <div class="column position">${positionComponent(row.position)}</div>
+      </div>
+      `;
   });
-  tableHtml += `</table>`;
+  tableHtml += `</div></div>`;
   return tableHtml;
 };
--- daw/js/helpers.js
+++ daw/js/helpers.js
@@ -13,3 +13,19 @@ const sortByKeyDesc = (array, key) => {
  * Genera un numero entre 1 y 10
  */
 const getRandom = () => (Math.floor(Math.random() * 10) + 1)
+
+/**
+ * Obtiene la clase de css en funcion de la posicion del coche
+ */
+const getClassPosition = (position) => {
+  switch (position) {
+    case 1:
+      return "first"
+    case 2:
+      return "second"
+    case 3:
+      return "third"
+    }
+    return "";
+}
+
