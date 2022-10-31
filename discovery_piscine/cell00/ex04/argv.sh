7d4cdbe64ca07f26a691924cd9f8f67d7d8e3ae5
Alberto Ramirez <57710147+arsalas@users.noreply.github.com>
2022-11-01 00:24:43 +0100

    updated

--- discovery_piscine/cell00/ex04/argv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ $# -lt 4 ]; then
-	for i in "$@"
-	do
-    	echo "$i"
-	done
-fi
--- discovery_piscine/cell03/ex00/main.js
+++
@@ -1,9 +0,0 @@
-const getRandom = () => Math.ceil(Math.random() * 255);
-
-const handleClick = () => {
-  document.querySelector(
-    "#body"
-  ).style.backgroundColor = `rgb(${getRandom()}, ${getRandom()}, ${getRandom()})`;
-};
-
-document.querySelector("#btn").addEventListener("click", handleClick, false);
