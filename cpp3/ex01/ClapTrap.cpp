c3a8dd06d86eda0d816eafae276853de2d456001
Alberto Ramirez <57710147+arsalas@users.noreply.github.com>
2022-11-01 00:13:55 +0100

    commit

---
+++ cpp3/ex01/ClapTrap.cpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramirez <aramirez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/09 13:16:25 by aramirez          #+#    #+#             */
+/*   Updated: 2022/05/09 13:30:50 by aramirez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string name)
+{
+	std::cout << "ClapTrap construct" << std::endl;
+	_name = name;
+}
+
+ClapTrap::~ClapTrap(void)
+{
+	std::cout << "ClapTrap destroy" << std::endl;
+}
+
+unsigned int ClapTrap::getHp(void)
+{
+	return (_hp);
+}
+
+unsigned int ClapTrap::getEp(void)
+{
+	return (_ep);
+}
+
+unsigned int ClapTrap::getAtk(void)
+{
+	return (_atk);
+}
+
+void ClapTrap::setHp(unsigned int hp)
+{
+	_hp = hp;
+}
+void ClapTrap::setEp(unsigned int ep)
+{
+	_ep = ep;
+}
+void ClapTrap::setAtk(unsigned int atk)
+{
+	_atk = atk;
+}
+
+void ClapTrap::attack(std::string const &target)
+{
+	std::cout << "ClapTrap " << _name << " attack " << target << std::endl;
+}
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	std::cout << "ClapTrap " << _name << " causing " << amount << " points of damage!" << std::endl;
+}
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	std::cout << "ClapTrap " << _name << " repaired " << amount << " points of damage!" << std::endl;
+}
---
+++ cpp3/ex01/ScavTrap.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramirez <aramirez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/09 13:42:30 by aramirez          #+#    #+#             */
+/*   Updated: 2022/05/09 20:12:47 by aramirez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+#include "ClapTrap.hpp"
+
+ScavTrap::ScavTrap(std::string name) : ClapTrap(name)
+{
+	std::cout << "ScravTrap construct" << std::endl;
+	ScavTrap::setHp(100);
+	ScavTrap::setEp(50);
+	ScavTrap::setAtk(20);
+}
+
+ScavTrap::~ScavTrap()
+{
+	std::cout << "ScravTrap destroy" << std::endl;
+}
+
+void ScavTrap::guardGate()
+{
+	std::cout << "ScravTrap have enterred in Gate keeper mode" << std::endl;
+}
