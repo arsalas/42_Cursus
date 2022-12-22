eb1a717ae9f165dd045079d58d3788cae252f1e7
Alberto Ramirez <57710147+arsalas@users.noreply.github.com>
2022-12-22 11:35:07 +0100

    net practice

--- cpp/cpp_4/ex01/Brain.hpp
+++ cpp/cpp_4/ex01/Brain.hpp
@@ -1,7 +1,6 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-<<<<<<<< HEAD:cpp_4/ex01/Brain.hpp
 /*   Brain.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aramirez <aramirez@student.42.fr>          +#+  +:+       +#+        */
@@ -26,32 +25,3 @@ public:
 };
 
 #endif
-========
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: aramirez <aramirez@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/18 12:34:48 by aramirez          #+#    #+#             */
-/*   Updated: 2022/05/24 17:50:36 by aramirez         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "push_swap.h"
-
-int	main(int argc, char *argv[])
-{
-	t_stack	stack_a;
-	t_stack	stack_b;
-
-	create_stacks(&stack_a, &stack_b, argc - 1);
-	if (do_format(argc, argv, &stack_a) == 1)
-		print_error();
-	get_positions(&stack_a);
-	if (stack_a.len <= 5)
-		order_radial(&stack_a, &stack_b, 1);
-	else
-		chunk_sort(&stack_a, &stack_b);
-	destroy_stacks(&stack_a, &stack_b);
-	return (0);
-}
->>>>>>>> 8ce7687 (update):push_swap/src/main.c
