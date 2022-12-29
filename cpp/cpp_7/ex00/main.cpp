5879401f52008988043e2d478b6a599646055e0c
Alberto Ramirez <57710147+arsalas@users.noreply.github.com>
2022-12-29 17:18:54 +0100

    fix

--- cpp/cpp_7/ex00/main.cpp
+++ cpp/cpp_7/ex00/main.cpp
@@ -100,5 +100,23 @@ int main(void)
 		std::cout << "min: " << ::min(s1, s2) << std::endl;
 		std::cout << std::endl;
 	}
+	{
+		std::cout << "==============================" << std::endl;
+		std::cout << "\tSUBJECT MAIN" << std::endl;
+		std::cout << "==============================" << std::endl;
+
+		int a = 2;
+		int b = 3;
+		::swap(a, b);
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+		std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+		std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+	}
 	return 0;
 }
