ba0cc20a0ecdda490be24aac6a6e90700c7ae3f9
Alberto Ramirez <57710147+arsalas@users.noreply.github.com>
2023-01-09 23:26:58 +0100

    Update main.cpp

--- cpp/cpp_6/ex01/src/main.cpp
+++ cpp/cpp_6/ex01/src/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include "Data.hpp"
 
-
 int main()
 {
 	Data data1;
-	data1.raw = "Lorem Ipsum";
+	data1.raw = "Hola Mundo";
 
 	uintptr_t raw = serialize(&data1);
 	Data *data2 = deserialize(raw);
 
-	std::cout << data2->raw << std::endl;
+	std::cout << "data1 -> " << data1.raw << std::endl;
+	std::cout << "data2 -> " << data2->raw << std::endl;
 }
