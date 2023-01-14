a596c207b5a471c38e8051cf77c6ba9fc82e0bfc
Alberto Ramirez <57710147+arsalas@users.noreply.github.com>
2023-01-14 16:34:35 +0100

    mariadb done

--- inception/srcs/requirements/mariadb/tools/init_db.sh
+++ inception/srcs/requirements/mariadb/tools/init_db.sh
@@ -1,26 +1,22 @@
 #!/bin/sh
-
-# Iniciamos mysql para poder ejecutar comandos
+# Iniciamos mysql para poder ejecutar comandos (si no ponemos el ; no lee bien el comando)
 service mysql start;
-
 # Creamos la tabla a partir de las variables de entorno
-mysql -e "CREATE DATABASE IF NOT EXIST \`${SQL_DATABASE}\`;"
-
+mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
 # Creamos un usuario
-mysql -e "CREATE USER IF NOT EXIST \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 # Damos derechos al usuario
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-
+# Recargarmos la tabla de privilegios
+mysql -e "FLUSH PRIVILEGES;"
 # Cambiamos el usuario root
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-
-# Recargarmos para que se apliquen los cambios
-mysql -e "FLUSH PRIVILIEGES;"
-
+# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 # Cerramos mysql
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-
-# Comando recomendado para mysql
-exec mysqld_safe
-
+# mysqladmin -u root shutdown;
+# service mysql restart;
+# Comando para iniciar mysql y que no se cierre el programa
+# exec mysqld_safe
+# Paramos mysql
+service mysql stop;
+# Ejecutamos el mysqld --bind-address=0.0.0.0 que tenemos en el CMD del Dockerfile
+exec "$@"
