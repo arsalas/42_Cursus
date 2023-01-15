c5c0649388d1d6218e3e9ac87a5d4973f9a5d882
Alberto Ramirez <57710147+arsalas@users.noreply.github.com>
2023-01-15 11:28:16 +0100

    Update init_db.sh

--- inception/srcs/requirements/mariadb/tools/init_db.sh
+++ inception/srcs/requirements/mariadb/tools/init_db.sh
@@ -7,8 +7,12 @@ mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_DATABASE};"
 mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
 # Damos derechos al usuario
 mysql -e "GRANT ALL PRIVILEGES ON \`${DB_DATABASE}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+# Le damos un password al root
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
 # Recargarmos la tabla de privilegios
 mysql -e "FLUSH PRIVILEGES;"
+# Cerramos el usuario root (con -p indicamos el password del usuario, tiene que ser -p seguido del pass sin espacios)
+mysqladmin -u root -p$DB_ROOT_PASSWORD shutdown
 # Paramos mysql
 service mysql stop;
 # Ejecutamos el mysqld --bind-address=0.0.0.0 que tenemos en el CMD del Dockerfile
