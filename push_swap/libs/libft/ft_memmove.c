094f59bdfa26dc16cc9c08825b833b205683ea2d
Alberto Ramirez <a.ramir.sa@gmail.com>
2023-02-20 17:39:50 +0100

    fix

--- push_swap/libs/libft/ft_memmove.c
+++ push_swap/libs/libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: aramirez <aramirez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 16:51:03 by aramirez          #+#    #+#             */
-/*   Updated: 2022/01/25 14:06:42 by aramirez         ###   ########.fr       */
+/*   Updated: 2023/02/13 16:14:42 by aramirez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	unsigned char	*s;
 	unsigned char	*d;
-	//size_t			i;
 
 	s = (unsigned char *)src;
 	d = (unsigned char *)dst;
-	//i = 0;
 	if (!dst && !src)
 		return (NULL);
 	if (dst > src)
